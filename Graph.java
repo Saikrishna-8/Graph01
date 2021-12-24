5955cc61f31236bb775fc951f7486dfbad71edf1
Saikrishna-8 <88083193+Saikrishna-8@users.noreply.github.com>
2021-12-24 22:18:19 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -23,7 +23,21 @@ public class Graph {
 		int V=8;
 		int E=8;
 		Graph g=new Graph(V,E);
-		g.addEdge(0,1);
+		/* 
+		
+		0
+		|
+		|
+		1----------2
+		|	   |
+		|	   |
+		3----------4---------5
+		|          |
+		|	   | 		
+		6	   7
+		                           */
+			
+		g.addEdge(0,1);              
 		g.addEdge(1,2);
 		g.addEdge(1,3);
 		g.addEdge(3,4);
